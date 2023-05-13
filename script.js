b358394a7a549b7b7e7cde3af27f40deff70b96e
ThinusvanStaden94 <109592693+ThinusvanStaden94@users.noreply.github.com>
2023-05-13 16:36:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// JavaScript code for the website
+
+// Function to handle the "Surprise Mom" button click event
+function surpriseMom() {
+    alert("Surprise! Happy Mother's Day, Mom!");
+  }
+  
+  // Add event listener to the "Surprise Mom" button
+  var surpriseBtn = document.getElementById("surpriseBtn");
+  surpriseBtn.addEventListener("click", surpriseMom);
